edaf1916c78a8fabe5865ac4e30029ef1a426f01
Michael Cooper <macoop2363@gmail.com>
2021-06-22 14:35:54 -0600

    Add client method to query the database

--- tap_xactly/client.py
+++ tap_xactly/client.py
@@ -1,8 +1,11 @@
+from typing import List, Dict
 import jaydebeapi
 from jaydebeapi import Connection
 
-# Build the Data Resource Service Here as a class with each endpoint as a function.
-# Do not iterate over paginated endpoints in this file.  Below are just samples
+
+import singer
+
+LOGGER = singer.get_logger()
 
 
 class XactlyClient:
@@ -27,3 +30,41 @@ class XactlyClient:
     @property
     def is_connected(self) -> bool:
         return not self._client.jconn.isClosed()
+
+    def query_database(
+        self,
+        table_name: str,
+        primary_key: str,
+        limit=1000,
+        offset=0,
+        limit_key="modified_date",
+        limit_key_value="1970-01-11T00:00:01Z",
+    ) -> List[Dict]:
+
+        row_count = 0
+
+        LOGGER.info("Querying DB")
+        self._sql.execute(
+            "SELECT * "
+            + f"FROM {table_name} "
+            + f"WHERE {limit_key} >= {limit_key_value} "
+            + f"ORDER BY {limit_key}, {primary_key} "
+            + f"LIMIT {limit} OFFSET {offset}"
+        )
+
+        LOGGER.info("Query Complete.  Starting rows")
+        rows = []
+
+        results = self._sql.fetchall()
+        columns = [key[0] for key in self._sql.description]
+
+        for row in results:
+            record = {}
+            for idx, val in enumerate(row):
+                if "java.time" in type(val).__name__:
+                    val = str(val)
+                record[columns[idx]] = val
+            rows.append(record)
+            row_count += 1
+        LOGGER.info(f"Row count = {row_count}")
+        return rows
