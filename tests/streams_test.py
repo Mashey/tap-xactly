f1ec274b300206e791c97e173ed29f193ba20667
Steven Anderson <steven.a.anderson2012@gmail.com>
2021-06-28 12:49:53 -0600

    Add endpoint and test for xc_pos_hierarchy_type_hist

--- tests/streams_test.py
+++ tests/streams_test.py
@@ -3,7 +3,8 @@ import pytest
 from tap_xactly.streams import (
     XcPosHierarchy,
     XcPosHierarchyHist,
-    XcPosHierarchytype,
+    XcPosHierarchyType,
+    XcPosHierarchyTypeHist,
     XcPosRelTypeHist,
     XcPosRelations,
     XcPosRelationsHist,
@@ -141,8 +142,14 @@ def xc_pos_hierarchy_hist_obj(client, state, catalog):
 
 @pytest.fixture
 def xc_pos_hierarchy_type_obj(client, state, catalog):
-    stream = catalog.get_stream(XcPosHierarchytype.tap_stream_id)
-    return XcPosHierarchytype(client, state, stream)
+    stream = catalog.get_stream(XcPosHierarchyType.tap_stream_id)
+    return XcPosHierarchyType(client, state, stream)
+
+
+@pytest.fixture
+def xc_pos_hierarchy_type_hist_obj(client, state, catalog):
+    stream = catalog.get_stream(XcPosHierarchyTypeHist.tap_stream_id)
+    return XcPosHierarchyTypeHist(client, state, stream)
 
 
 def test_xc_pos_rel_type_hist(xc_pos_rel_type_hist_obj):
@@ -400,4 +407,15 @@ def test_xc_pos_hierarchy_type(xc_pos_hierarchy_type_obj):
     assert xc_pos_hierarchy_type_obj.replication_key == "MODIFIED_DATE"
 
     assert "xc_pos_hierarchy_type" in STREAMS
-    assert STREAMS["xc_pos_hierarchy_type"] == XcPosHierarchytype
+    assert STREAMS["xc_pos_hierarchy_type"] == XcPosHierarchyType
+
+
+def test_xc_pos_hierarchy_type_hist(xc_pos_hierarchy_type_hist_obj):
+    assert xc_pos_hierarchy_type_hist_obj.tap_stream_id == "xc_pos_hierarchy_type_hist"
+    assert xc_pos_hierarchy_type_hist_obj.key_properties == ["POS_HIERARCHY_TYPE_ID"]
+    assert xc_pos_hierarchy_type_hist_obj.object_type == "XC_POS_HIERARCHY_TYPE_HIST"
+    assert xc_pos_hierarchy_type_hist_obj.valid_replication_keys == ["MODIFIED_DATE"]
+    assert xc_pos_hierarchy_type_hist_obj.replication_key == "MODIFIED_DATE"
+
+    assert "xc_pos_hierarchy_type_hist" in STREAMS
+    assert STREAMS["xc_pos_hierarchy_type_hist"] == XcPosHierarchyTypeHist
